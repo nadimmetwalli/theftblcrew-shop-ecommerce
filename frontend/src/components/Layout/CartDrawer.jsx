dbdde7dacc24de05f7122d03741ccb8a970df88f
Nadim Metwalli <nadimmetwalli@gmail.com>
2025-07-27 00:43:32 +0300

    Cart adjustments

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { IoMdClose } from "react-icons/io";
 import { useState } from 'react'
+import CartContents from '../Cart/CartContents';
 
 
 
 const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
     
   return (
-    <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50
+    <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50
     ${drawerOpen ? "translate-x-0" : "translate-x-full"
     }`}
     >
@@ -22,8 +23,9 @@ const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
     {/* Cart content with scrollable area*/}
     <div className='flex-grow p-4 overflow-y-auto'>
         <h2 className='text-xl font-semibold mb-4'>My cart</h2>
-        {/* Component for cart contents */}
+        <CartContents />
     </div>
+
     {/* Checkout button fixed at the bottom */}
     <div className='p-4 bg-white sticky bottom-0'>
         <button className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition'>Checkout</button>
